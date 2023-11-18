94d03494ef09f3a41ad39d488751cd6d51f24f3a
Suraj Kumar Gupta <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2023-11-19 03:26:31 +0530

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const  dbConnect=async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://vignamInternSuraj:FPAj8iBEfp8UAvj5@vignamcluster.iotehtj.mongodb.net/?retryWrites=true&w=majority");
         console.log("DB Connected Successfully")
     } catch (error) {
         console.log("DB Connection failed", error.message);
